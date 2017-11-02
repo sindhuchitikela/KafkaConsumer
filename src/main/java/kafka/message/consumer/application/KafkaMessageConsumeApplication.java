8d7968ad9f9f5319299132e59b792cfd3618d03a
Sindhu.Chitikela <Sindhu.Chitikela@target.com>
2017-11-02 15:03:50 -0500

    cleanup

--- src/main/java/kafka/message/consumer/application/KafkaMessageConsumeApplication.java
+++ src/main/java/kafka/message/consumer/application/KafkaMessageConsumeApplication.java
@@ -1,4 +1,4 @@
-package com.tgt.omt.offer.consume;
+package kafka.message.consumer.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- src/main/java/kafka/message/consumer/service/OfferChangeConsumerService.java
+++ src/main/java/kafka/message/consumer/service/OfferChangeConsumerService.java
@@ -1,4 +1,4 @@
-package com.tgt.omt.offer.consume.service;
+package kafka.message.consumer.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
